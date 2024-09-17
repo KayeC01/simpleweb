e5a56e4c904f64780393a06938b119d9014b6b12
KayeC01 <cardenascassandrakaye@gmail.com>
2024-09-18 05:27:54 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+import numpy as np
+import os
+
+app = Flask (__name__)
+
+app.config['UPLOAD_FOLDER'] = 'static/'
+model = pickle.load(open("predictor.pkl", "rb"))
+
+@app.route('/')
+def home ():
+    pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'image.jpg')
+    return render_template("index.html", image=pic1)
+
+@app.route('/predic', methods=['GET', 'POST'])
+def predict():
+    Gender = int(request.form ["Gender"])
+    Age = int(request.form ["Age"])
+    Height = float(request.form ["Height"])
+    Weight = float(request.form ["Weight"])
+    Duration = float(request.form ["Duration"])
+    Heart_Rate = float(request.form ["Heart_Rate"])
+    Body_Temp = float(request.form ["Body_Temp"])
+
+input_data = (Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp)
+input_data_as_numpy_array = np.asarray(input_data)
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+prediction = model.predict(input_data_reshaped)[0]
+prediction = round(prediction)
+return = stp(prediction)
+
